da876ca26d7415bd75a458d5a667a3f3ab592ab3
Miguel Ángel León Bardavío <mlb0029@alu.ubu.es>
2017-10-02 21:09:03 +0200

    2/10/2017
    www
    Logica
    TestLogica

---
+++ Logica/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica
+{
+    public class Usuario
+    {
+        public Usuario(int _idUsuario, string _cuenta, string _eMail)
+        {
+            this.idUsusario = _idUsuario;
+            this.cuenta = _cuenta;
+            this.eMail = _eMail;
+        }
+
+        private int idUsusario;
+        public int IdUsuario
+        {
+            get { return this.idUsusario; }
+            set { this.idUsusario = value; }
+        }
+
+        private string cuenta;
+        public string Cuenta
+        {
+            get { return this.cuenta; }
+            set { this.cuenta = value; }
+        }
+
+        private string eMail;
+        public string Email
+        {
+            get { return this.eMail; }
+            set { this.eMail = value; }
+        }
+
+
+    }
+}
